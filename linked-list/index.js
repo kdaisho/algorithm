fe8bcac484e4d4ab319e9913193d0357defefc92
Daisho Komiyama <daishokomiyama@gmail.com>
2022-06-04 23:52:13 -0400

    Add insert and unshift methods

--- linked-list/index.js
+++ linked-list/index.js
@@ -20,12 +20,44 @@ class LinkedList {
     this.tail = node
   }
 
+  unshift(value) {
+    const node = new Node(value)
+    this.length++
+
+    const oldHead = this.head
+    this.head = node
+    this.head.next = oldHead
+
+    if (this.tail === null) {
+      this.tail = node
+    }
+  }
+
+  insert(index, value) {
+    if (index > this.length || index < 0) return null
+    const node = new Node(value)
+    this.length++
+
+    const prev = this._find(index - 1)
+    if (index === 0) {
+      // index is 0 so this would be just like this.unshift()
+      this.unshift(node)
+    } else if (prev.next === null) {
+      // this means index equals to this.length so just like this.push()
+      this.push(value)
+    } else {
+      const oldNext = prev.next
+      prev.next = node
+      node.next = oldNext
+    }
+  }
+
   pop() {
     return this.delete(this.length - 1)
   }
 
   _find(index) {
-    if (index >= this.length) return null
+    if (index >= this.length || index <= -1) return null
     let cur = this.head
     for (let i = 0; i < index; i++) {
       cur = cur.next
@@ -75,13 +107,13 @@ class Node {
 
 const linkedList = new LinkedList()
 
-linkedList.push(3)
-linkedList.push(5)
 linkedList.push(7)
-// linkedList.push(9)
+linkedList.unshift(5)
+linkedList.unshift(10)
+linkedList.insert(3, 15)
+linkedList.insert(3, 25)
+linkedList.push(0)
 
 // console.log('deleted =>', linkedList.delete(0))
-// console.log('deleted =>', linkedList.delete(0))
-// console.log('deleted =>', linkedList.delete(1))
 
 console.log('linkedList =>', JSON.stringify(linkedList, null, 2))
